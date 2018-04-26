4624556543c62634e12023ebc5cfe6e3da1d9cb4
Dennis Lord <lorddennis1006@gmail.com>
2018-04-26 21:48:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ WIN_COMBINATIONS = [
   def input_to_index(user_input)
     index = user_input.to_i - 1
   end
-  def valid_move?(board, index)
+  def valid_move?(board, index, token)
      if index.between?(0,8) && !position_taken?(board, index)
        true
      else
