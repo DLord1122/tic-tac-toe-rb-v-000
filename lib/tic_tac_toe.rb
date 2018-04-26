48c4e9cab43ebf4dd02abf745d7d23b26db96290
Dennis Lord <lorddennis1006@gmail.com>
2018-04-26 21:02:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,6 +57,15 @@ WIN_COMBINATIONS = [
       turn(board)
     end
   end
+  def turn_count(board)
+  number_of_turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      number_of_turns += 1
+    end
+  end
+  return number_of_turns
+end
    def position_taken?(board, location)
     board[location] != " "
    end
